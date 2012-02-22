545c2dc39dab3f00e5a9b05c3d5b6612cb809ab0
masiuchi <masahiro.iuchi@gmail.com>
2012-02-22 22:40:03 +0900

    add hatebu filter

--- plugins/HatebuCount/lib/HatebuCount/ListProps.pm
+++ plugins/HatebuCount/lib/HatebuCount/ListProps.pm
@@ -4,10 +4,12 @@ use strict;
 sub list_prop {
     return {
         hatebu_count => {
-            label           => 'Hatebu Count',
-            display         => 'default',
-            raw             => \&_raw,
-            bulk_sort       => \&_bulk_sort,
+            base      => '__virtual.integer',
+            label     => 'Hatebu Count',
+            display   => 'default',
+            raw       => \&_raw,
+            bulk_sort => \&_bulk_sort,
+            terms     => \&_terms,
         },
     };
 }
@@ -20,6 +22,46 @@ sub _raw {
 sub _bulk_sort {
     my ( $prop, $objs ) = @_;
     return sort { $a->hatebu_count <=> $b->hatebu_count } @$objs;
+}
+
+sub _terms {
+    my $prop = shift;
+    my ( $args, $db_terms, $db_args ) = @_;
+
+    my $option = $args->{option};
+    my $value  = $args->{value};
+    my $query;
+    if ( 'equal' eq $option ) {
+        $query = $value;
+    }
+    elsif ( 'not_equal' eq $option ) {
+        $query = { not => $value };
+    }
+    elsif ( 'greater_than' eq $option ) {
+        $query = { '>' => $value };
+    }
+    elsif ( 'greater_equal' eq $option ) {
+        $query = { '>=' => $value };
+    }
+    elsif ( 'less_than' eq $option ) {
+        $query = { '<' => $value };
+    }
+    elsif ( 'less_equal' eq $option ) {
+        $query = { '<=' => $value };
+    }
+
+    $db_args->{joins} ||= [];
+    require MT::Entry;
+    push @{ $db_args->{joins} }, MT::Entry->meta_pkg()->join_on(
+        undef,
+        {
+            entry_id => \'= entry_id',
+            type     => 'hatebu_count',
+            vinteger => $query,
+        },
+        {
+        },
+    );
 }
 
 1;
